7b9bd3c9485927b9fe9c783717d8893babe8024a
Vikas Kumar <vikkumar@ntdev.microsoft.com>
2026-07-06 11:48:15 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Text from './components/text'
+
+
+function App() {
+    return (
+        <div>
+            <Text />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/text.tsx
@@ -0,0 +1,42 @@
+
+import { animate, stagger } from 'animejs';
+import { useEffect, useRef } from 'react';
+//import { text } from 'animejs';
+
+
+function Text() {
+	const textRef = useRef(null);
+	const text = "HAPPY BIRTHDAY!"; 
+
+	useEffect(() => {
+		const els = textRef.current?.querySelectorAll('.balloon');
+		if (!els || !els.length) return;
+
+		animate(els, {
+			y: '100cqh',
+			loopDelay: 1000,
+			loop: true,
+			delay: stagger(100),
+			from: 1,
+			to: 0.14,
+			duration: 3000,
+			ease: 'inOutSine'
+
+		});
+	}, []
+	);
+	
+	return (
+		<div className="container"ref={textRef}>
+		{text.split("").map((char, index) => (
+			<div key={index}>
+				{char === " " ? null : <div className="balloon">{char}</div>}
+			</div>
+		))}
+	</div>
+
+	
+	)
+}
+
+export default Text;
